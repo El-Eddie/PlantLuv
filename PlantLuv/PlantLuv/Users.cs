c10386e4bc486c697dae89f635ea951762ce1b0d
El Butler <elvia@outlook.com>
2020-03-10 17:16:54 -0500

    Added Users.cs

--- PlantLuv/PlantLuv/Users.cs
+++ PlantLuv/PlantLuv/Users.cs
@@ -1,11 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace PlantLuv
 {
-    class Users
+    public class Users
     {
-
+        public int UserID { get; set; }
+        [MinLength(4), MaxLength(50)]
+        [Required()]
+        public string Username { get; set; }
+        [MinLength(6),MaxLength(50)]
+        [DataType(DataType.Password)]
+        [Required()]
+        public string Password { get; set; }
+        public string Avatar { get; set; }
+        public int DateCreated { get; set; }
     }
 }
+
