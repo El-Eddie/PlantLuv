fa7e1f9d60d06b1a463dbc5e0339b3e9d863abfe
EddieTheLovableTramp <61154544+EddieTheLovableTramp@users.noreply.github.com>
2020-03-10 15:13:31 -0500

    Creating PlatLuv project. Created PlantType and LightLevel classes.

---
+++ PlantLuv/PlantLuv/PlantType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PlantLuv
+{
+	public class PlantType
+	{
+		public int PlantId { get; set; }
+		public string LatinName { get; set; }
+		public string CommonName { get; set; }
+		public double HumidityLevel { get; set; }
+		public LightLevel LightLevel { get; set; }
+		public int LightTime { get; set; }
+		public double SoilPh { get; set; }
+
+	}
+}
