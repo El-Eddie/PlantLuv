e062d4b1e6e246faca8702ae12463524540b24c5
El Butler <elvia@outlook.com>
2020-07-31 12:39:08 -0500

    Pc Switch

--- PlantLuv/PlantLuv-ng/src/app/app.component.ts
+++ PlantLuv/PlantLuv-ng/src/app/app.component.ts
@@ -1,23 +1,31 @@
 import { Component } from '@angular/core';
 import { Store, select } from '@ngrx/store';
 import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-
-
 })
+
+
+
 export class AppComponent {
   title = 'PlantLuv';
   updateFilter$: Observable<boolean>; //should this be an observable?
 
-  updateFilter(thing: string){
+  constructor(private router: Router) { }
+
+  updateFilter(thing: string) {
     // TODO: implement function to search and pass results to DashboardComponent
   }
   openUserSettings() {
     // TODO: Create and call user settings page/dialog box
+  }
+  userLogout() {
+    localStorage.removeItem('userToken');
+    this.router.navigate(['plantluv.net'])
   }
 }
