d8b3f7b090d8a80f219b2a58dd4a4d3eca154507
El Butler <elvia@outlook.com>
2020-08-14 17:17:52 -0500

    Added plant type pop up

--- PlantLuv/PlantLuv-ng/src/app/dashboard/plant-type-add-new-plant/plant-type-add-new-plant.component.ts
+++ PlantLuv/PlantLuv-ng/src/app/dashboard/plant-type-add-new-plant/plant-type-add-new-plant.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
-import { MatDialogRef } from '@angular/material/dialog';
+import { Component, OnInit, Inject } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, AbstractControl, ValidationErrors } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { PlantType } from '../models/plant-type.model';
+import { of, Observable } from 'rxjs';
 
 
 @Component({
@@ -10,10 +12,45 @@ import { MatDialogRef } from '@angular/material/dialog';
 })
 export class PlantTypeAddNewComponent implements OnInit {
   newPlantType: FormGroup;
+  selectedtypeId: number;
+  typeList: string[] = [];
+  typeList$: Observable<PlantType[]>;
+  filteredTypeList$: Observable<string[]>;
+  otherOption: string = "other/unlisted"
 
   constructor(
-    public dialogRef: MatDialogRef<PlantTypeAddNewComponent>
-  ) { }
+    public dialogRef: MatDialogRef<PlantTypeAddNewComponent>,
+    public builder: FormBuilder,
+    @Inject(MAT_DIALOG_DATA) public type: PlantType | null
+  ) {
+    var selectedPlant = (this.type && this.type.commonName) ? this.type.commonName : '';
+    this.newPlantType = this.builder.group({
+      latinName: [''],
+      commonName: [''],
+      description: [''],
+      difficultyLeve: [''],
+      toxicTo: [''],
+      humidityLow: [''],
+      humidityHigh: [''],
+      lightLevel: [''],
+      lightTime: [''],
+      soilPh: [''],
+      soilType: [''],
+      fertalizerFrequency: [''],
+      fertalizerType: [''],
+      wateringFrequency: [''],
+      wateringType: [''],
+      kingdom: [''],
+      clade1: [''],
+      clade2: [''],
+      clade3: [''],
+      order: [''],
+      family: [''],
+      subFamily: [''],
+      genus: [''],
+    })
+  }
+
 
   ngOnInit(): void {
   }
